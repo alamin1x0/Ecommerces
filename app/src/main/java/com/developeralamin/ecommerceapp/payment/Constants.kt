2f2720b86a76d740f1effde54c5e4b8ce4ff90a5
Webns_Sakib <info.webnstech@gmail.com>
2025-01-24 21:15:58 +0600

    Admin

--- app/src/main/java/com/developeralamin/ecommerceapp/payment/Constants.kt
+++ app/src/main/java/com/developeralamin/ecommerceapp/payment/Constants.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.MutableLiveData
 object Constants {
 
     //test
-    var bkashSandboxUsername = "sandboxTokenizedUser02"
+/*    var bkashSandboxUsername = "sandboxTokenizedUser02"
     var bkashSandboxPassword = "sandboxTokenizedUser02@12345"
     var bkashSandboxAppKey = "4f6o0cjiki2rfm34kfdadl1eqq"
     var bkashSandboxAppSecret = "2is7hdktrekvrbljjh44ll3d9l1dtjo4pasmjvs5vl5qr3fug4b"
@@ -17,7 +17,7 @@ object Constants {
     var amount = "10"
     var currency = "BDT"
     var intents = "sale"
-    var merchantInvoiceNumber = "Inv0124"
+    var merchantInvoiceNumber = "Inv0124"*/
 
     //Live
 
